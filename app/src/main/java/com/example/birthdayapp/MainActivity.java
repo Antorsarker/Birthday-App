4c1d4453ff93470c8ce6bef4c8003e939366caf5
Antorsarker <antors1996@gmail.com>
2020-01-01 18:08:03 +0600

    second commit

--- app/src/main/java/com/example/birthdayapp/MainActivity.java
+++ app/src/main/java/com/example/birthdayapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.birthdayapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameEdittext,birthdayEditText;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameEdittext=findViewById(R.id.nameID);
+        birthdayEditText=findViewById(R.id.birthdayID);
+        saveButton=findViewById(R.id.saveButtonID);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name=nameEdittext.getText().toString();
+                String birthday=birthdayEditText.getText().toString();
+
+                Toast.makeText(MainActivity.this, "save"+name+" "+birthday, Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
     }
+
+
 }
